f770e56dd9acde3c5724c4dc76c51d2c06ba4853
Sang Hyuk Lee <sanghlee@hotmail.ca>
2018-06-24 19:17:00 -0400

    Add 1st source

---
+++ TestLocWpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace TestLocWpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (TranslationSource.Instance.CurrentCulture == null)
+                TranslationSource.Instance.CurrentCulture = new CultureInfo("fr");
+            else
+                TranslationSource.Instance.CurrentCulture = null;
+        }
+    }
+}
---
+++ TestLocWpfApp1/TranslationSource.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Resources;
+using System.Collections.Generic;
+
+namespace TestLocWpfApp1
+{ 
+    class TranslationSource : INotifyPropertyChanged
+    {
+        private const char KeySeparator = '_';
+        private static readonly TranslationSource instance = new TranslationSource();
+
+        public static TranslationSource Instance
+        {
+            get { return instance; }
+        }
+
+        private readonly ResourceManager resManager = Properties.Resources.ResourceManager;
+        private CultureInfo currentCulture = null;
+
+        public string this[string key]
+        {
+            
+            get
+            {
+                if (key.IndexOf(KeySeparator) == -1)
+                {
+                    var res = this.resManager.GetString(key, this.currentCulture);
+                    return !string.IsNullOrWhiteSpace(res) ? res : $"[{key}]";
+                }
+                else
+                {
+                    var keys = key.Split(KeySeparator);
+                    var results = new List<string>();
+                    foreach (var k in keys)
+                    {
+                        var res = this.resManager.GetString(k, this.currentCulture);
+                        results.Add(!string.IsNullOrWhiteSpace(res) ? res : $"[{k}]");
+                    }
+                    return string.Join(" ", results);
+                }
+            }
+        }
+
+        public CultureInfo CurrentCulture
+        {
+            get { return this.currentCulture; }
+            set
+            {
+                if (this.currentCulture != value)
+                {
+                    this.currentCulture = value;
+                    var @event = this.PropertyChanged;
+                    if (@event != null)
+                    {
+                        @event.Invoke(this, new PropertyChangedEventArgs(string.Empty));
+                    }
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
